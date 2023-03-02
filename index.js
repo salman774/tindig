b2ce5e7a4b772870b5133d3c0410ff7cdee7cff7
salman naveed <realmadridinmydna3@gmail.com>
2023-03-02 13:50:56 +0500

    array lenght feature added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { dogs } from "./data.js";
 let teddyboy = new Dogsdata(dogs.teddy);
 let bellagirl = new Dogsdata(dogs.bella);
 let rexboy = new Dogsdata(dogs.rex);
+let button = false;
 
 let arrayofdogs = [teddyboy, bellagirl, rexboy];
 let arrayholdingdog = [];
@@ -13,7 +14,6 @@ for (let i = 0; i < 1; i++) {
 }
 arrayholdingdog = arrayholdingdog[0];
 console.log(arrayholdingdog);
-// let arrayholdingdog = [arrayofdogs.shift()][0];
 
 const render = () => {
   document.getElementById("image-container").innerHTML =
@@ -22,33 +22,38 @@ const render = () => {
 render();
 
 const like = () => {
-  arrayholdingdog.likefunction();
-
-  newdogarray();
-  // console.log(arrayholdingdog.hasBeenLiked);
-  // console.log(arrayholdingdog.hasBeenSwiped);
+  if (!button) {
+    arrayholdingdog.likefunction();
+    newdogarray();
+  }
 };
 
 const unlike = () => {
-  arrayholdingdog.unlikefunction();
-  // console.log(arrayholdingdog.hasBeenLiked);
-  // console.log(arrayholdingdog.hasBeenSwiped);
+  if (!button) {
+    arrayholdingdog.unlikefunction();
+    newdogarray();
+  }
 };
 
 document.getElementById("unliked").addEventListener("click", like);
 document.getElementById("liked").addEventListener("click", unlike);
 
 const newdogarray = () => {
-  for (let i = 0; i < 1; i++) {
-    arrayholdingdog = [];
-    let array = arrayofdogs.shift();
-    arrayholdingdog.push(array);
-    arrayholdingdog = arrayholdingdog[0];
-    console.log(arrayholdingdog);
-    arrayholdingdog.gethtmlofdogsdata();
+  if (arrayofdogs.length > 0) {
+    for (let i = 0; i < 1; i++) {
+      arrayholdingdog = [];
+      let array = arrayofdogs.shift();
+      arrayholdingdog.push(array);
+      arrayholdingdog = arrayholdingdog[0];
+      console.log(arrayholdingdog);
+      arrayholdingdog.gethtmlofdogsdata();
+      button = true;
+    }
+  } else {
+    console.log("array ended");
   }
-
   setTimeout(() => {
+    button = false;
     render();
   }, 2000);
 };
